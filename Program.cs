be11aec29671429ddf2ead092eb31f2bc1fb9a23
Giggbit <danya19981017@gmail.com>
2023-02-07 12:43:31 +0100

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labwork_07._02._2023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            City city = new City();
+            city.Enter();
+            city.Print();
+
+        }
+    }
+}
